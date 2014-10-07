f9b2b2d1aef88eb5d49914d5cd01532bb6cdcaa4
Yonashiro Yuu <cloneko@gmail.com>
2014-10-08 02:35:44 +0900

    最後の議事録になった時に空っぽのデータを返して終了の判断をさせるようにした。

--- collector.py
+++ collector.py
@@ -6,6 +6,12 @@ class Collector:
     def __init__(self,hostname,firstpath):
         self.hostname = hostname
         self.firstpath = firstpath 
+        self.titlePattern = '<td width="66%" bgcolor="#CCCCFF">((昭和|平成).*?)</td>'
+        self.datePattern = '<td width="66%" bgcolor="#CCCCFF">(?:昭和|平成).*</td><td width="34%" bgcolor="#CCCCFF"><div align="right">(.*?)</div></td>'
+        self.speakerPattern = '<tr valign="top"><td width="66%" bgcolor="#CCCCFF">(.*)</td><td width="34%" bgcolor="#CCCCFF"><img width="1" height="1" src="/icons/ecblank.gif" border="0" alt=""></td></tr>'
+        self.bodyPattern = '<HR width=98%>(.*)<HR width=98%>'
+        self.prevPattern = '<a href="(.*&amp;To=Prev)">'
+        self.nextPattern = '<a href="(.*&amp;To=NextMain)">' 
 
 
     def get(self,path = ''):
@@ -18,25 +24,16 @@ class Collector:
 
     def parser(self,content):
 
-        titlePattern = '<td width="66%" bgcolor="#CCCCFF">((昭和|平成).*?)</td>'
-        datePattern = '<td width="66%" bgcolor="#CCCCFF">(?:昭和|平成).*</td><td width="34%" bgcolor="#CCCCFF"><div align="right">(.*?)</div></td>'
-        speakerPattern = '<tr valign="top"><td width="66%" bgcolor="#CCCCFF">(.*)</td><td width="34%" bgcolor="#CCCCFF"><img width="1" height="1" src="/icons/ecblank.gif" border="0" alt=""></td></tr>'
-        bodyPattern = '<HR width=98%>(.*)<HR width=98%>'
-        prevPattern = '<a href="(.*&amp;To=Prev)">'
-        nextPattern = '<a href="(.*&amp;To=NextMain)">' 
 
-        title = re.search(titlePattern,content).group(1)
-        date = re.search(datePattern,content).group(1) if re.search(datePattern,content) != None else ''
-        speaker = re.search(speakerPattern,content).group(1) if re.search(speakerPattern,content)  != None else ''
-        body = re.search(bodyPattern,content,re.MULTILINE + re.DOTALL).group(1)
-        prevUrl = re.search(prevPattern,content).group(1)
-        nextUrl = re.search(nextPattern,content).group(1)
-        obj = {'title': title,'date': date,'speaker': speaker,'body': body,'prev': prevUrl,'next': nextUrl}
-        #print(title)
-        #print(date)
-        #print(speaker)
-        #print(body)
-        #print(prevUrl)
-        #print(nextUrl)
+        try:
+            title = re.search(self.titlePattern,content).group(1)
+            date = re.search(self.datePattern,content).group(1) if re.search(self.datePattern,content) != None else ''
+            speaker = re.search(self.speakerPattern,content).group(1) if re.search(self.speakerPattern,content)  != None else ''
+            body = re.search(self.bodyPattern,content,re.MULTILINE + re.DOTALL).group(1)
+            prevUrl = re.search(self.prevPattern,content).group(1)
+            nextUrl = re.search(self.nextPattern,content).group(1)
+            obj = {'title': title,'date': date,'speaker': speaker,'body': body,'prev': prevUrl,'next': nextUrl}
+        except AttributeError:
+            obj = {'title': '','date': '','speaker': '','body': '','prev': '','next': ''}
 
         return obj
